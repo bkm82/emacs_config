b959d17bed5da1f46330ccc4a28bb93b20d95ab2
Bray Moll <bkm82@nau.edu>
2023-09-23 17:11:10 -0700

    add python to org-babbel, update R config to match python format

--- init.el
+++ init.el
@@ -354,6 +354,7 @@
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((R . t)
+     (python .t)
      (org . t)
      (ditaa . t)
      (latex . t)
@@ -470,8 +471,46 @@
 (use-package lsp-ivy
   :after lsp)
 
+(use-package dap-mode
+  ;; Uncomment the config below if you want all UI panes to be hidden by default!
+  ;; :custom
+  ;; (lsp-enable-dap-auto-configure nil)
+  ;; :config
+  ;; (dap-ui-mode 1)
+  :commands dap-debug
+  :config
+  ;; Set up Node debugging
+  (require 'dap-node)
+  (dap-node-setup) ;; Automatically installs Node debug adapter if needed
+
+  ;; Bind `C-c l d` to `dap-hydra` for easy access
+  (general-define-key
+    :keymaps 'lsp-mode-map
+    :prefix lsp-keymap-prefix
+    "d" '(dap-hydra t :wk "debugger")))
+
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  (python-shell-interpreter "python3")
+  (dap-python-executable "python3")
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+(use-package pyvenv
+  :after python-mode
+  :config
+  (pyvenv-mode 1))
+
 (setq ess-r-backend 'lsp)
-(add-hook 'ess-r-mode-hook (lambda () (lsp)))
+;;(add-hook 'ess-r-mode-hook (lambda () (lsp)))
+
+(use-package ess-r-mode
+    :ensure nil
+    :hook (ess-r-mode . lsp-deferred))
 
 
 
